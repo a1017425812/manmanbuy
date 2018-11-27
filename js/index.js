e69f0cbd50ffd7a8b074b9ae674c9b9d439eaf1a
monicaicai <604809883@qq.com>
2018-11-27 15:42:39 +0800

    更新头部和底部

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 $(function () {
 	var mmb = new MMB();
-	mmb.getIndexMenu();
-	mmb.getDiscountProduct();
+	// mmb.getIndexMenu();
+	// mmb.getDiscountProduct();
+	mmb.scrooltop($('.top'));
+
 });
 
 var MMB = function () {
@@ -10,24 +12,33 @@ var MMB = function () {
 MMB.prototype = {
 	baseURL:'http://localhost:9090',
 	//获取首页菜单的函数
-	getIndexMenu:function () {
-		$.ajax({
-			url:this.baseURL+'/api/getindexmenu',
-			success:function (data) {
-				var html = template('indexMenuTpl',data);
-				$('#nav .mui-row').html(html);
-			}
-		})
-	},
+	// getIndexMenu:function () {
+	// 	$.ajax({
+	// 		url:this.baseURL+'/api/getindexmenu',
+	// 		success:function (data) {
+	// 			var html = template('indexMenuTpl',data);
+	// 			$('#nav .mui-row').html(html);
+	// 		}
+	// 	})
+	// },
 	//获取首页折扣商品
-	getDiscountProduct:function () {
-		$.ajax({
-			url:this.baseURL+'/api/getmoneyctrl',
-			success:function (data) {
-				console.log(data);
-				var html = template('discountProductTpl',data);
-				$('#productList .content ul').html(html);
-			}
-		})
+	// getDiscountProduct:function () {
+	// 	$.ajax({
+	// 		url:this.baseURL+'/api/getmoneyctrl',
+	// 		success:function (data) {
+	// 			console.log(data);
+	// 			var html = template('discountProductTpl',data);
+	// 			$('#productList .content ul').html(html);
+	// 		}
+	// 	})
+	// },
+	scrooltop:function(btn){
+		var btn = $(btn);
+		btn.onscorll = function(){
+			console.log(111);
+			
+            document.body.scrollTop = document.documentElement.scrollTop = 500;
+		}
+
 	}
 }
